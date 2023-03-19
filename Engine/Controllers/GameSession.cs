22bc75caec44dc14db9d15d9afe373d0a2f3d833
varesh007 <varesh007@DESKTOP-OFUA444>
2023-03-19 17:16:06 +0400

    Added View for Controller created

---
+++ Engine/Controllers/GameSession.cs
@@ -0,0 +1,34 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Controllers
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            InitialiseNewSession(CurrentPlayer);
+        }
+
+        public GameSession(Player player)
+        {
+            CurrentPlayer = player;
+        }
+
+        private void InitialiseNewSession(Player player) {
+            player.Name = "Varesh";
+            player.CharacterClass = "Figher";
+            player.Gold = 0;
+            player.Level = 1;
+            player.HitPoints = 100;
+            player.ExperiencePoints = 0;
+        }
+    }
+}
--- WpfUI/MainWindow.xaml.cs
+++ WpfUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Engine.Controllers;
 
 namespace WpfUI
 {
@@ -20,9 +21,14 @@ namespace WpfUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GameSession _gameSession;
         public MainWindow()
         {
             InitializeComponent();
+
+            _gameSession = new GameSession();
+
+            DataContext = _gameSession;
         }
     }
 }
